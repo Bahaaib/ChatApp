4a0da8dfc3d8e272b4514c8a56c33abf967da0d7
Bahaaib <Bahaasco@gmail.com>
2019-05-01 02:51:01 +0200

    designed Login Activity

--- app/src/main/java/com/example/bahaa/chatapp/Auth/LoginActivity.java
+++ app/src/main/java/com/example/bahaa/chatapp/Auth/LoginActivity.java
@@ -1,13 +1,15 @@
-package com.example.bahaa.chatapp;
+package com.example.bahaa.chatapp.Auth;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.bahaa.chatapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
