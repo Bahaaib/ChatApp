1f7978c3855b128befa530a59bf41897744df789
Bahaaib <Bahaasco@gmail.com>
2019-05-01 06:18:34 +0200

    created group model

---
+++ app/src/main/java/com/example/bahaa/chatapp/Root/GroupModel.java
@@ -0,0 +1,18 @@
+package com.example.bahaa.chatapp.Root;
+
+public class GroupModel {
+
+    private String title;
+
+    public GroupModel() {
+        //Required empty public constructor
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
